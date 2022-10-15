6d45cd65a02d01dd514121e7dfa1a43e4fc45949
ekaterina <kostcat-84@mail.ru>
2022-10-15 22:53:02 +0300

    все четные

---
+++ zadanie4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("введите число больше 0");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+if (numberA >= 2)
+{
+    while (i <= numberA)
+    {
+        if (i%2 == 0)
+        {
+            Console.Write(i);
+            Console.Write(";");
+        }
+        
+        i+=1;
+        
+    } 
+}
+else
+{
+    Console.WriteLine("введите число побольше");
+}
