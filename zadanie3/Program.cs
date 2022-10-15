053ccba7b460e6d02d601534f6a786a29e52ccfe
ekaterina <kostcat-84@mail.ru>
2022-10-15 22:18:13 +0300

    чётное

---
+++ zadanie3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA%2 == 0)
+{
+    Console.Write(numberA);
+    Console.Write(" - чётное число");
+}
+else
+{
+    Console.Write(numberA);
+    Console.Write(" - не чётное число");
+}
