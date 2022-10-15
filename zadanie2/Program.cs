b4eca34f4e7a69fce823885c4c8a40f921ce292e
ekaterina <kostcat-84@mail.ru>
2022-10-15 22:05:43 +0300

    максимальное из 3

---
+++ zadanie2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("введите три числа");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.Write(max);
+Console.Write(" - наибольшее число");
