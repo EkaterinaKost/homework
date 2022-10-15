08544c1468778326295bbca941ef83b929bbe497
ekaterina <kostcat-84@mail.ru>
2022-10-15 21:48:55 +0300

    какое больше

---
+++ zadanie1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("введите первое число");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число");
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+  Console.Write("первое число больше" );
+}
+
+else
+{
+    if (numberB > numberA)
+    {
+         Console.Write("второе число больше");
+    }
+
+     else
+     {
+      Console.Write("числа равны");  
+     }
+ }
+   
