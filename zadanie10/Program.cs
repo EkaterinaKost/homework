a80fb2d1a9ef98c1808c559d8789a8b6f9110fc2
ekaterina <kostcat-84@mail.ru>
2022-10-16 21:44:01 +0300

    enter second number

---
+++ zadanie10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите трёхзначное число");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+int result = ShowSecondNumber(numberA);
+Console.WriteLine(result);
+
+int ShowSecondNumber(int number)
+{
+    number = number / 10;
+    number = number % 10;
+    return number;
+}
