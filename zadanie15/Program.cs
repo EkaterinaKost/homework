d792180607030fbff6293b6fdc29068679477a19
ekaterina <kostcat-84@mail.ru>
2022-10-22 21:05:29 +0300

    день недели

---
+++ zadanie15/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("введите число, обозначающее день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = Weekend(number);
+
+Console.WriteLine(result);
+bool Weekend(int numberWeek)
+{
+    return numberWeek == 6 | numberWeek == 7;
+}
