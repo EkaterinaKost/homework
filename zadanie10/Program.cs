c0a79dfdb80f7afc5e706bedba573316576402bd
ekaterina <kostcat-84@mail.ru>
2022-10-22 21:29:19 +0300

    вторая цифра

--- zadanie10/Program.cs
+++ zadanie10/Program.cs
@@ -1,12 +1,21 @@
 ﻿Console.WriteLine("введите трёхзначное число");
 
 int numberA = Convert.ToInt32(Console.ReadLine());
-int result = ShowSecondNumber(numberA);
+int result=ShowSecondNumber(numberA);
 Console.WriteLine(result);
 
 int ShowSecondNumber(int number)
 {
-    number = number / 10;
-    number = number % 10;
-    return number;
+    if (number / 100 == 0 | number/1000!=0)
+    {
+
+        Console.WriteLine("ошибка");
+    
+    }
+    
+    number=number/10;
+    number=number%10;
+
+    return number; 
+     
 }
