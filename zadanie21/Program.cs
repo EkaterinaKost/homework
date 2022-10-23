2abbd05d70aa3d4141ad6805409516440626f595
ekaterina <kostcat-84@mail.ru>
2022-10-23 15:04:25 +0300

    расстояние в 3д

---
+++ zadanie21/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите координаты точек");
+bool isParsedXA = int.TryParse(Console.ReadLine(), out int xaa);
+bool isParsedYA = int.TryParse(Console.ReadLine(), out int yaa);
+bool isParsedZA = int.TryParse(Console.ReadLine(), out int zaa);
+bool isParsedXB = int.TryParse(Console.ReadLine(), out int xbb);
+bool isParsedYB = int.TryParse(Console.ReadLine(), out int ybb);
+bool isParsedZB = int.TryParse(Console.ReadLine(), out int zbb);
+if(!isParsedXA|| !isParsedYA || !isParsedXB|| 
+   !isParsedYB ||!isParsedZA || !isParsedZB)
+{
+    Console.WriteLine("Ошибка! Не правильно введены координаты!!!!");
+    return;
+}
+
+double distAB = DistanceAB (xaa,yaa,zaa, xbb, ybb,zbb);
+Console.WriteLine (distAB);
+double DistanceAB(int xa, int ya,int za, int xb, int yb, int zb)
+{
+    double dist = Math.Sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya)+(zb-za)*(zb-za));
+    return dist;
+}
