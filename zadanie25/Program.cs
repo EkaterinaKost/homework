1068aeb28634ee32f3474f4474647cccf4b62b18
ekaterina <kostcat-84@mail.ru>
2022-10-25 20:19:04 +0300

    возведение А в степень В

---
+++ zadanie25/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("введите первое число");
+bool isParsedA = int.TryParse(Console.ReadLine(), out int a);
+Console.WriteLine("введите второё число");
+bool isParsedB = int.TryParse(Console.ReadLine(), out int b);
+
+if (!isParsedA && !isParsedB)
+{
+    Console.WriteLine("Ошибка! введено не число!!!!");
+    return;
+}
+int result = ExponentiationAB(a, b);
+Console.WriteLine(result);
+
+int ExponentiationAB(int numbera, int numberb)
+{
+     if (numberb < 0)
+    {
+        numberb = -numberb;
+    }
+   int exp=1;
+    for (int i = 1; i <= numberb; i++)
+    {
+        exp=exp*numbera;
+    }
+    return exp;
+}
