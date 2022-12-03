d9e366356bd38d7843838c30e765ff9c4193b850
ekaterina <kostcat-84@mail.ru>
2022-12-03 13:01:32 +0300

    outputs all natural numbers in the range from N to 1

---
+++ zadanie641/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 64: Задайте значение N. Напишите метод, 
+который выведет все натуральные числа в промежутке от N до 1.
+ Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+Console.WriteLine("введите число n");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedN)
+{
+    Console.WriteLine("Ошибка! введено не число!!!!");
+    return;
+}
+PrintNumbers(n);
+void PrintNumbers(int n,int i=1)
+{
+    if (n<i)
+    {
+        return;
+    }
+    Console.Write($"{n} ");
+    PrintNumbers(n-1,i);
+}
