bb8b0af5d2ac506c0e23227336b85329991dbabb
ekaterina <kostcat-84@mail.ru>
2022-12-03 12:31:55 +0300

    calculating the Ackerman function

---
+++ zadanie681/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 68: Напишите метод вычисления функции 
+Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+Console.WriteLine("введите не отрицательное число натуральное число m");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("введите не отрицательное число n");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка! введено не число!!!!");
+    return;
+}
+Console.WriteLine(ColculatingTheAckermanFunction(m, n));
+int ColculatingTheAckermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if ((m != 0) && (n == 0))
+        {
+            return ColculatingTheAckermanFunction(m - 1, 1);
+        }
+        else
+        {
+            return ColculatingTheAckermanFunction(m - 1, ColculatingTheAckermanFunction(m, n - 1));
+
+        }
+
+    }
+}
